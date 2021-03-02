e0e4d2a88a1addae7dad1b34f6f9c236515b7ef0
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-02 13:08:00 -0500

    Expectation for ArticleBox component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './../App';
+import ArticleBox from './../ArticleBox';
+import ArticleList from '../ArticleList';
 
-it('shows a Article Box', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// it('shows a Article Box', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
 
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
+//   // Looks inside the div
+//   // and checks to see if the CommentBox is in there
   
-  // console.log(div.innerHTML); // <div><div>Article Box</div><div>Article List</div></div>
+//   // console.log(div.innerHTML); // <div><div>Article Box</div><div>Article List</div></div>
 
-  expect(div.innerHTML).toContain('Article Box');
+//   expect(div.innerHTML).toContain('Article Box');
 
-  ReactDOM.unmountComponentAtNode(div);
+//   ReactDOM.unmountComponentAtNode(div);
+// })
+
+
+it('shows a ArticleBox', () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(ArticleBox).length).toEqual(1);
 })
